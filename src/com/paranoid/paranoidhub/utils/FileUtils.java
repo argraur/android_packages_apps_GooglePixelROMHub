61634d6596cd6853777220fca84dc5876ea46a39
Arseniy Graur <argraur12@yandex.ru>
2017-06-09 13:36:38 +0300

    Create FileUtils.java

--- src/com/paranoid/paranoidhub/utils/FileUtils.java
+++ src/com/paranoid/paranoidhub/utils/FileUtils.java
@@ -34,10 +34,10 @@ import java.util.Scanner;
 public class FileUtils {
 
     public static final String DOWNLOAD_PATH = new File(Environment
-            .getExternalStorageDirectory(), "paranoidhub/").getAbsolutePath();
+            .getExternalStorageDirectory(), "pixelromota/").getAbsolutePath();
     private static final String SDCARD = Environment.getExternalStorageDirectory()
             .getAbsolutePath();
-    private static final String PREFIX = "pa_";
+    private static final String PREFIX = "GooglePixelROM_";
     private static final String SUFFIX = ".zip";
 
     private static String sPrimarySdcard;
